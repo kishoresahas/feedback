01ee9b0bebd33fceecb3d79fbb575e5276fe8fde
Kishore Sahasranaman <kishore.sahasranaman@bs-shipmanagement.com>
2018-02-13 11:20:13 +0530

    Added Sumit request path

--- feedback.js/feedback.js/server.js
+++ feedback.js/feedback.js/server.js
@@ -3,6 +3,10 @@ var app = express();
 app.get('/', function (req, res) {
     res.redirect('/index.html');
 });
+app.post('/submit', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 1 }));
+});
 app.listen(3000, function () { return console.log('Example app listening on port 3000!'); });
 app.use(express.static('public'));
 //# sourceMappingURL=server.js.map
--- feedback.js/feedback.js/server.ts
+++ feedback.js/feedback.js/server.ts
@@ -5,5 +5,10 @@ app.get('/', function (req, res) {
 	res.redirect('/index.html');
 });
 
+app.post('/submit', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	res.send(JSON.stringify({ a: 1 }));
+});
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 app.use(express.static('public'))
